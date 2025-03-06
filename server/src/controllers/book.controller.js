71b1704abf459214ba2a8baf433a323320a879c6
TranMinhKhangg <khangb2203506@student.ctu.edu.vn>
2025-03-06 21:00:03 +0700

    cap nhat them du lieu

---
+++ server/src/controllers/book.controller.js
@@ -0,0 +1,106 @@
+const bookService = require('../services/book.service')
+const jwt = require('jsonwebtoken')
+const ApiError = require('../api-error')
+
+
+function verifyToken (req, res) {
+    const token = req.headers['authorization']
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, process.env.JWT_SECRET || 'B2203510_CT449_HKI2024-2025', (error, user) => {
+            if( error || !user.ChucVu) {
+                return reject('Unauthorized !')
+            }
+            else {
+                resolve(user)
+            }
+        }) 
+    })
+}
+
+// [GET] [/book]
+module.exports.getAll = async (req, res, next) =>{
+    try {
+        const bookservice = new bookService()
+        const result = await bookservice.getAll()
+        res.json(result)
+    } catch (error) {
+        console.log(error)
+        return next( new ApiError(500, "An error occurred while getAll books !")
+        )
+    }
+}
+
+// [POST] [/book/add]
+module.exports.add =  async (req, res, next) =>{
+    if(!req.body.TenSach) {
+        return next( new ApiError(400, "Tên sách không được để trống!"))
+    }
+    try {
+        await verifyToken(req, res) 
+        const bookservice = new bookService()
+        const result = await bookservice.add(req.body)
+        res.json(result)
+    } catch (error) {
+        console.log(error)
+        if(error == 'Unauthorized !') {
+            return next( new ApiError(401,error))
+        }
+        return next( new ApiError(500, "An error occurred while adding book !"))
+    }
+}
+
+// [PATCH] [/book/update/:MaSach]
+module.exports.update = async (req, res, next) =>{
+    try {
+        await verifyToken(req,res)
+        const bookservice = new bookService()
+        const result =  await bookservice.update(req.body)
+        res.json(result)
+    } catch (error) {
+        console.log(error)
+        if(error == 'Unauthorized !') {
+            return next( new ApiError(401,error))
+        }
+        return next( new ApiError(500, "An error occurred while updating book !"))
+    }
+}
+
+// [DELETE] [/book/delete/:MaSach]
+module.exports.delete = async (req, res, next) =>{
+    try {
+        await verifyToken(req,res)
+        const bookservice = new bookService()
+        const result =await  bookservice.delete(req.params.MaSach)
+        if(!result) {
+            return next ( new ApiError (404, "Không tìm thấy sách!"))
+        }
+        return res.json({
+            result,
+            message:"Xóa sách thành công!"
+        })
+    } catch (error) {
+        console.log(error)
+        if(error == 'Unauthorized !') {
+            return next( new ApiError(401,error))
+        }
+        return next( new ApiError(500, "An error occurred while updating book !"))
+    }
+}
+
+// [DELETE] [book/delete/]
+module.exports.deleteAll = async (req, res,  next) =>{
+    try {
+        await verifyToken(req,res)
+        const bookservice = new bookService()
+        const deletedCount = await bookservice.deleteAll()
+        res.json({
+            message: `${deletedCount} sách được xóa thành công!`
+        })
+    } catch (error) {
+        console.log(error)
+        if(error == 'Unauthorized !') {
+            return next( new ApiError(401,error))
+        }
+        return next( new ApiError(500, "An error occurred deleting all books !"))
+    }
+}
---
+++ server/src/controllers/publisher.controller.js
@@ -0,0 +1,120 @@
+const publisherService = require('../services/publisher.service')
+const jwt = require('jsonwebtoken')
+const ApiError = require('../api-error')
+
+function verifyToken (req, res) {
+    const token = req.headers['authorization']
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, process.env.JWT_SECRET || 'B2203510_CT449_HKI2024-2025', (error, user) => {
+            if( error || !user.ChucVu ) { 
+                return reject('Unauthorized !')
+            }
+            else {
+                resolve(user)
+            }
+        }) 
+    })
+}
+// [POST] [publisher/add]
+module.exports.add =  async (req, res, next ) =>{
+    if(!req.body?.TenNXB) {
+        return next ( new ApiError(400, "Tên nhà xuất bản không được để trống!"))
+    }
+    try {
+        await verifyToken(req, res)
+        const publisher = new publisherService()
+        const result =  await publisher.add(req.body)
+        res.json(result)
+
+    } catch (error) {
+        console.log(error)
+        if (error == 'Unauthorized !'){
+            return next( new ApiError(401, error))
+        }
+        return next( new ApiError(500, "An error occurred while Add Publisher !"))
+    }
+}
+
+
+//[PATCH] /publisher/update/:MaNXB
+module.exports.update = async (req, res , next) =>{
+    try {
+        await verifyToken(req, res) 
+        const publisher = new publisherService()
+        const result = await publisher.update( req.body)
+        if(!result) {
+            return next ( new ApiError(404, "Không tìm thấy nhà xuất bản!") )
+        }
+        return res.json(result)
+    } catch (error) {
+        console.log(error)
+        if(error == 'Unauthorized !') {
+            return next ( new ApiError(401, error))
+        } else {
+            return next( new ApiError(500, "An error occurred while update !"))
+        }
+    }
+}
+
+
+//[DELETE] /publisher/delete/:MaNXB
+module.exports.delete =  async (req, res, next ) =>{
+    try {
+        await verifyToken(req, res) 
+        const publisher = new publisherService()
+        const result = await publisher.delete(req.params.MaNXB)
+        if(!result) {
+            return next(new ApiError (404, "Không tìm thấy nhà xuất bản!"))
+        }
+        return res.json(result)
+    } catch (error) {
+        console.log(error)
+        if(error == 'Unauthorized !') {
+            return next ( new ApiError(401, error))
+        } else {
+            return next( new ApiError(500, "An error occurred while delete !"))
+        }
+    }
+}
+
+//[GET] /publisher/
+module.exports.getAll = async ( req, res, next) =>{
+    try {
+        await verifyToken(req, res)
+        const publisher = new publisherService()
+        const {TenNXB} = req.query
+        if(!TenNXB) {
+            const result = await publisher.find({})
+            res.json(result)
+        } else {
+            const result = await publisher.findByName(TenNXB)
+            res.json(result)
+        }
+    } catch (error) {
+        console.log(error)
+        if(error == 'Unauthorized !') {
+            return next ( new ApiError(401, error))
+        } else {
+            return next( new ApiError(500, "An error occurred while getAll !"))
+        }
+    }
+}
+
+//[DELETE] publisher/deleteAll
+module.exports.deleteAll = async ( req, res, next) =>{
+    try {
+        await verifyToken(req, res)
+        const publisher = new publisherService()
+        const deleteCount = await publisher.deleteAll()
+        res.json({
+            message: `${deleteCount} Nhà xuất bản được xóa thành công!`
+        })
+    } catch (error) {
+        console.log(error)
+        if(error == 'Unauthorized !') {
+            return next ( new ApiError(401, error))
+        } else {
+            return next( new ApiError(500, "An error occurred while deleteAll !"))
+        }
+    }
+}
